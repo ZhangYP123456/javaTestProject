a97672acb5ccd0802787dd43d48c23aef2c67068
张云鹏 <1837662734@qq.com>
2021-12-02 22:06:11 +0800

    1

--- src/main/java/Test01.java
+++ src/main/java/Test01.java
@@ -2,5 +2,8 @@ public class Test01 {
 
     public static void main(String[] args) {
         System.out.println("hahaha");
+        int[] test1 = {1,12,33};
+
+        System.out.println(test1[0]);
     }
 }
